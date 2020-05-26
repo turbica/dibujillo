bc9a864d8f89e6bd73d4eea425ff894960691696
Turbica <723883@unizar.es>
2020-05-26 12:42:03 +0200

    Travis

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,13 +5,12 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:dibujillo/main.dart';
 
 import 'FirebaseUserTest.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   FirebaseUserTest();
 }
